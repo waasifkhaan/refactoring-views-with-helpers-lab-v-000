97324cf698059b92b485e2721c4dc7adf1165d73
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 04:17:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  
+   def artist_name
+    self.artist.name
+  end
+
+  def artist_name=(name)
+    self.artist.name = name 
+    
+  end 
 
 end
