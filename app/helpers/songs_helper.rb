f56e33dffbad2db0849bb1cb8b1d35575b03e826
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 04:26:27 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,13 +1,14 @@
 module SongsHelper
   
-  def artist_name
-    self.artist.name
+   def artist_name
+    if self.artist
+      self.artist.name
+    end 
   end
 
   def artist_name=(name)
     if self.artist
       self.artist.name = name
-    else 
     end 
   end 
 
